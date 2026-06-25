bd02023b3556d129d2a855edeb2644be148fad5f
AryHamdanHelmy <ahamdan24@gmail.com>
2026-06-25 19:00:50 +0700

    save

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -7,9 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Invitation extends Model
 {
     protected $fillable = [
-    'slug', 'groom_name', 'groom_parents', 'groom_photo',
-    'bride_name', 'bride_parents', 'bride_photo', 'cover_photo',
-    'quote_text', 'quote_source', 'music_url', 'theme',
+        'slug',
+        'groom_name', 
+        'groom_parents', 
+        'groom_photo',
+        'bride_name', 
+        'bride_parents', 
+        'bride_photo', 
+        'cover_photo',
+        'quote_text', 
+        'quote_source', 
+        'music_url', 
+        'theme',
     ];
     public function events(){
         return $this->hasMany(Event::class);
